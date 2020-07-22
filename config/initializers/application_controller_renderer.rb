d5e147f575a049b8e07d1db2ed50752bed0bae74
Ryota-1 <stream.of.tears@icloud.com>
2020-07-22 15:58:36 +0900

    課題提出

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'fa1f3427b34cf1fa190e62e7bf09c00359331240a85ae9e5b631386ef8a9d6aed5f7d2f4ad5572be46f9d258536bac7e99e3da5de27961bfe49e8e2c83de07f0'
