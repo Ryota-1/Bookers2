c7cce2b1089691413b36ac2d4156d69bc840dd41
Ryota-1 <stream.of.tears@icloud.com>
2020-08-05 13:20:05 +0900

    フォロー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   get 'home/about'
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users do
+  	resources :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   resources :books, except: [:new] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
